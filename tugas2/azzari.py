d418e003e7d232b9a203657a7ba43cd0389e1e07
azahri <azahri.22315026@umkendari.ac.id>
2023-10-23 18:04:36 +0700

    tugas 2 selesai

---
+++ tugas2/azzari.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+	# tupple
+	x = ( 1, 'Belimbing' , 3.14 , 'ceri')
+	# list
+	y = [ 1, 'Belimbing' , 3.14 , 'ceri']
+	# dictionery
+	z = {'a': 'Apel' , 'b': 'Belimbing' , 'c': 'ceri' , 'x': 'xemangka'}	
+	print(type(x))
+	print(x)
+	print(type(y))
+	print(y)
+	print(type(z))
+	print(z)
